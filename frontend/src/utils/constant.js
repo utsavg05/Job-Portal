2fd8a46823495bd7437f9ca72f427532943d8940
utsavg05 <utsav.gp1204@gmail.com>
2025-06-11 15:14:24 +0530

    Refactor: Update API endpoints to use production base URL and comment out local endpoints

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,12 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user"
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user"
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company"
+
+
+const BASE_URL = "https://jobseek-ete7.onrender.com/api/v1";
+
+export const USER_API_END_POINT = `${BASE_URL}/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
